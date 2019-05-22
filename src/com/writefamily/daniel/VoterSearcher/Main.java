15b624aadd4f9647815780559690819908f67ed1
Daniel Write <daniel@writefamily.com>
2019-05-22 18:14:30 -0400

    Rebirthed critical program-scope information
    Signed-off-by: Daniel Write <daniel@writefamily.com>

--- src/com/writefamily/daniel/VoterSearcher/Main.java
+++ src/com/writefamily/daniel/VoterSearcher/Main.java
@@ -17,7 +17,29 @@
 
 package com.writefamily.daniel.VoterSearcher;
 
+import com.writefamily.daniel.VoterSearcher.scheduling.TaskScheduler;
+
 public class Main {
+    public static final TaskScheduler TASK_SCHEDULER = new TaskScheduler(Runtime.getRuntime().availableProcessors());
+    public static final String OVR_BASE_URL = "https://www6.sos.state.oh.us/ords/f?p=VOTERFTP:DOWNLOAD::FILE:NO:2:P2_PRODUCT_NUMBER:%d";
+    public static final String[] COUNTY_ARRAY = {
+            "ADAMS", "ALLEN", "ASHLAND", "ASHTABULA", "ATHENS", "AUGLAIZE",
+            "BELMONT", "BROWN", "BUTLER", "CARROLL", "CHAMPAIGN", "CLARK",
+            "CLERMONT", "CLINTON", "COLUMBIANA", "COSHOCTON", "CRAWFORD",
+            "CUYAHOGA", "DARKE", "DEFIANCE", "DELAWARE", "ERIE", "FAIRFIELD",
+            "FAYETTE", "FRANKLIN", "FULTON", "GALLIA", "GEAUGA", "GREENE",
+            "GUERNSEY", "HAMILTON", "HANCOCK", "HARDIN", "HARRISON", "HENRY",
+            "HIGHLAND", "HOCKING", "HOLMES", "HURON", "JACKSON", "JEFFERSON",
+            "KNOX", "LAKE", "LAWRENCE", "LICKING", "LOGAN", "LORAIN",
+            "LUCAS", "MADISON", "MAHONING", "MARION", "MEDINA", "MEIGS",
+            "MERCER", "MIAMI", "MONROE", "MONTGOMERY", "MORGAN", "MORROW",
+            "MUSKINGUM", "NOBLE", "OTTAWA", "PAULDING", "PERRY", "PICKAWAY",
+            "PIKE", "PORTAGE", "PUTNAM", "RICHLAND", "ROSS", "SANDUSKY",
+            "SCIOTO", "SENECA", "SHELBY", "STARK", "SUMMIT", "TRUMBULL",
+            "TUSCARAWAS", "UNION", "VANWERT", "VINTON", "WARREN", "WASHINGTON" +
+            "WAYNE", "WILLIAMS", "WOOD", "WYANDOT"
+    };
+
     public static void main(String[] args) {
 
     }
