8802de048145c8e1a07f49940af8f90a630a2d2c
Daniel Write <daniel@writefamily.com>
2019-05-25 17:19:44 -0400

    Removed dead code in analysis
    Signed-off-by: Daniel Write <daniel@writefamily.com>

--- src/com/writefamily/daniel/VoterSearcher/analysis/CSVAnalyzer.java
+++ src/com/writefamily/daniel/VoterSearcher/analysis/CSVAnalyzer.java
@@ -29,51 +29,7 @@ import java.util.*;
  * This class analyzes the CVS files and figures data ranges as to speed up search time
  */
 public class CSVAnalyzer {
-    public static Map<String, Map<String, Set<Map.Entry<Long, CSVRecord>>>> analyze(Reader reader) throws IOException {
-        CSVFormat vfrFormat = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim();
-        CSVParser csvParser = new CSVParser(reader, vfrFormat);
-
-        //   TYPE        KEY                  INDEX RECORD
-        Map<String, Map<String, Set<Map.Entry<Long, CSVRecord>>>> rawAnalysis = new HashMap<>();
-        rawAnalysis.put("LAST_NAME", new HashMap<>());
-        rawAnalysis.put("FIRST_NAME", new HashMap<>());
-        rawAnalysis.put("BIRTH_YEAR", new HashMap<>());
-        rawAnalysis.put("PARTY", new HashMap<>());
-        rawAnalysis.put("CITY", new HashMap<>());
-        for (CSVRecord record : csvParser.getRecords()) {
-            String lastName = record.get("LAST_NAME").split("")[0];
-            String firstName = record.get("FIRST_NAME").split("")[0]; // names are indexed by first letter alone
-            String birthYear = record.get("DATE_OF_BIRTH").split("-")[0];
-            String party = record.get("PARTY_AFFILIATION");
-            String city = record.get("RESIDENTIAL_CITY");
-            long recordIndex = record.getRecordNumber();
-            if (!rawAnalysis.get("LAST_NAME").containsKey(lastName)) {
-                rawAnalysis.get("LAST_NAME").put(lastName, new HashSet<>());
-            }
-            if (!rawAnalysis.get("FIRST_NAME").containsKey(firstName)) {
-                rawAnalysis.get("FIRST_NAME").put(firstName, new HashSet<>());
-            }
-            if (!rawAnalysis.get("BIRTH_YEAR").containsKey(birthYear)) {
-                rawAnalysis.get("BIRTH_YEAR").put(birthYear, new HashSet<>());
-            }
-            if (!rawAnalysis.get("PARTY").containsKey(party)) {
-                rawAnalysis.get("PARTY").put(party, new HashSet<>());
-            }
-            if (!rawAnalysis.get("CITY").containsKey(city)) {
-                rawAnalysis.get("CITY").put(city, new HashSet<>());
-            }
-
-            rawAnalysis.get("LAST_NAME").get(lastName).add(new AbstractMap.SimpleEntry<>(recordIndex, record));
-            rawAnalysis.get("FIRST_NAME").get(firstName).add(new AbstractMap.SimpleEntry<>(recordIndex, record));
-            rawAnalysis.get("BIRTH_YEAR").get(birthYear).add(new AbstractMap.SimpleEntry<>(recordIndex, record));
-            rawAnalysis.get("PARTY").get(party).add(new AbstractMap.SimpleEntry<>(recordIndex, record));
-            rawAnalysis.get("CITY").get(city).add(new AbstractMap.SimpleEntry<>(recordIndex, record));
-        }
-
-        return rawAnalysis;
-    }
-
-    public static CSVAnalysis learn(Reader reader) throws IOException {
+    public static CSVAnalysis analyze(Reader reader) throws IOException {
         CSVFormat vfrFormat = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim();
         CSVParser csvParser = new CSVParser(reader, vfrFormat);
 
