c0c226c63ff6fd2986474156cd37bdf94d96671c
Daniel Write <daniel@writefamily.com>
2019-06-01 00:54:12 -0400

    Blank slate for Main
    Signed-off-by: Daniel Write <daniel@writefamily.com>

--- src/com/writefamily/daniel/VoterSearcher/Main.java
+++ src/com/writefamily/daniel/VoterSearcher/Main.java
@@ -17,26 +17,9 @@
 
 package com.writefamily.daniel.VoterSearcher;
 
-import com.writefamily.daniel.VoterSearcher.analysis.AnalysisQuery;
-import com.writefamily.daniel.VoterSearcher.analysis.CSVAnalysis;
-import com.writefamily.daniel.VoterSearcher.analysis.CSVAnalyzer;
 import com.writefamily.daniel.VoterSearcher.scheduling.TaskScheduler;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
 
-import java.io.*;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
 
 public class Main {
     public static final TaskScheduler TASK_SCHEDULER = new TaskScheduler(Runtime.getRuntime().availableProcessors());
@@ -61,89 +44,5 @@ public class Main {
     public static final File BASE_DIR = new File("datas" + File.separator);
 
     public static void main(String[] args) {
-        List<String> firstName = new ArrayList<>(), lastName = new ArrayList<>(), birthYear = new ArrayList<>(),
-                party = new ArrayList<>(), city = new ArrayList<>(), county = new ArrayList<>();
-        for (int i = 0; i < args.length; i++) {
-            String[] arg = args[i].split("=");
-            String key = arg[0];
-            String val = arg[1];
-            switch (key) {
-                case "FIRST_NAME":
-                    firstName.addAll(Arrays.asList(val.split(";")));
-                    break;
-                case "LAST_NAME":
-                    lastName.addAll(Arrays.asList(val.split(";")));
-                    break;
-                case "BIRTH_YEAR":
-                    birthYear.addAll(Arrays.asList(val.split(";")));
-                    break;
-                case "PARTY":
-                    party.addAll(Arrays.asList(val.split(";")));
-                    break;
-                case "CITY":
-                    city.addAll(Arrays.asList(val.split(";")));
-                    break;
-                case "COUNTY":
-                    county.addAll(Arrays.asList(val.split(";")));
-                    break;
-            }
-        }
-
-        if (county.size() == 0) {
-            county.addAll(Arrays.asList(COUNTY_ARRAY));
-        }
-
-        int[] usingCounties = new int[county.size()];
-        for (int i = 0; i < usingCounties.length; i++) {
-            String needle = county.get(i);
-            for (int j = 0; j < COUNTY_ARRAY.length; j++) {
-                if (needle.equalsIgnoreCase(COUNTY_ARRAY[j])) {
-                    usingCounties[i] = j;
-                }
-            }
-        }
-
-        for (int i = 0; i < usingCounties.length; i++) {
-            int countyCode = usingCounties[i] + 1;
-            // task scheduling causes a too high memory load to be worth the increase in efficiency
-            //Main.TASK_SCHEDULER.scheduleTask(() -> {
-            try {
-                SSLContextBuilder builder = new SSLContextBuilder();
-                builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-                SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
-
-                CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-                HttpGet httpGet = new HttpGet(Main.OVR_BASE_URL + countyCode);
-                CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-
-                ByteArrayOutputStream store = new ByteArrayOutputStream();
-                int b;
-                while ((b = httpResponse.getEntity().getContent().read()) != -1) {
-                    store.write(b);
-                }
-                byte[] data = store.toByteArray();
-                CSVAnalysis analysis = CSVAnalyzer.analyze(new InputStreamReader(new ByteArrayInputStream(data)));
-                AnalysisQuery query = analysis.filter("FIRST_NAME", firstName.toArray(new String[0]))
-                        .filter("LAST_NAME", lastName.toArray(new String[0]))
-                        .filter("BIRTH_YEAR", birthYear.toArray(new String[0]))
-                        .filter("PARTY", party.toArray(new String[0]))
-                        .filter("CITY", city.toArray(new String[0]));
-                for (CSVRecord record : query.getRecords()) {
-                    System.out.println(CSVAnalyzer.formatRecord(record));
-                    System.out.println();
-                }
-
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            } catch (KeyStoreException e) {
-                e.printStackTrace();
-            } catch (KeyManagementException e) {
-                e.printStackTrace();
-            }
-            //});
-        }
     }
 }
